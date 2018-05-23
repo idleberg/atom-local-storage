3d27ba1264935b32fda2a64c48572dc65bc2c202
idleberg <jan@idleberg.com>
2018-05-23 20:33:35 +0200

    remove unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const coffeelint = require('gulp-coffeelint');
 const debug = require('gulp-debug');
 const gulp = require('gulp');
 const jsonlint = require('gulp-jsonlint');
-const lesshint = require('gulp-lesshint');
 const stylish = require('coffeelint-stylish');
 
 // Files
@@ -23,41 +22,27 @@ const jsonFiles = [
     './package.json'
 ];
 
-const lessFiles = [
-    './styles/**/*.less'
-];
-
 // Lint CoffeeScript & CSON files
-gulp.task('lint:coffee', gulp.series((done) => {
+gulp.task('lint:coffee', (done) => {
     gulp.src(coffeeFiles)
         .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter(stylish))
         .pipe(coffeelint.reporter('fail'));
     done();
-}));
-
-// Lint LESS files
-gulp.task('lint:less', gulp.series((done) => {
-    gulp.src(lessFiles)
-        .pipe(debug({title: 'lesshint:'}))
-        .pipe(lesshint())
-        .pipe(lesshint.reporter());
-    done();
-}));
+});
 
 // Lint JSON files
-gulp.task('lint:json', gulp.series((done) => {
+gulp.task('lint:json', (done) => {
     gulp.src(jsonFiles)
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
         .pipe(jsonlint.failAfterError())
         .pipe(jsonlint.reporter());
     done();
-}));
+});
 
 // Tasks
-gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', 'lint:less', (done) => {
+gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', done => {
   done();
 }));
-
